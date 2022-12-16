8d325fa4bb970d98b3a2bc34f33f25764cf2c0e0
Emigrek <kaioshinbox@gmail.com>
2022-12-16 06:49:15 +0100

    fixes

--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -4,6 +4,5 @@ export const fetchProjects = async() => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`);
     const data = await res.json();
     const projects: Project[] = data.projects;
-    console.log(projects[0].skills);
     return projects; 
 }
