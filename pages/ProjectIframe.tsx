5add3da7d994896f3b9a090b1106f14459feaf11
Emigrek <kaioshinbox@gmail.com>
2022-12-21 03:56:23 +0100

    Fixed project readme preview & snap scroll

--- pages/ProjectIframe.tsx
+++ pages/ProjectIframe.tsx
@@ -3,37 +3,50 @@ import { useRecoilValue } from 'recoil'
 import { pageState } from '../atoms/page';
 import { projectIframeState } from '../atoms/projectIframe';
 import NoProjectSelected from './NoProjectSelected';
-import NoProjectUrl from './NoProjectUrl';
 import ProjectReadme from './ProjectReadme';
 
 function ProjectIframe() {
-  const [loading, setLoading] = useState(true);
+  const [showReadme, setShowReadme] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(true);
   const projectIframe = useRecoilValue(projectIframeState);
   const page = useRecoilValue(pageState);
+  var readmeTimeout: NodeJS.Timeout | null = null;
   
-  const handleLoad = () => {
-    var loadTimeout = null;
-    loadTimeout = setTimeout(() => {
+  useEffect(() => {
+    if(readmeTimeout)
+      clearTimeout(readmeTimeout as NodeJS.Timeout);
+    
+    setShowReadme(true);
+    readmeTimeout = setTimeout(() => {
+      setShowReadme(false);
       setLoading(false);
     }, 3000);
-  }
 
-  useEffect(() => {
-    setLoading(true);
+    return () => {
+      clearTimeout(readmeTimeout as NodeJS.Timeout);
+    };
   }, [projectIframe]);
+
+  const handleIframeLoad = () => {
+    setLoading(false);
+  }
 
   if (!projectIframe) return <NoProjectSelected />
 
-  if (!projectIframe?.url) return <NoProjectUrl />
+  if (!projectIframe?.url) return (
+    <div className="w-full h-[85%] lg:h-[90%]">
+      <ProjectReadme/>
+    </div>
+  )
 
   return (
     <div className={`
       transition-all aspect-video w-full ${page.nav ? 'h-[85%] lg:h-[90%]' : 'h-[100%]'}
     `}>
-      {loading ? (
+      {showReadme || loading ? (
         <ProjectReadme/>
       ) : ''}
-      <iframe onLoad={handleLoad} loading={'lazy'} src={projectIframe?.url} className="relative w-full h-full"></iframe>
+      <iframe onLoad={handleIframeLoad} loading={'lazy'} src={projectIframe?.url} className="relative w-full h-full bg-black"></iframe>
     </div>
   )
 }
--- pages/ProjectReadme.tsx
+++ pages/ProjectReadme.tsx
@@ -22,21 +22,24 @@ function ProjectReadme() {
             {
                 data ? (
                     <ReactMarkdown 
-                        className="relative lg:w-1/3 w-full h-full text-white xl:p-8 p-2"
+                        className="relative lg:w-1/3 h-[85%] lg:h-[90%] w-full text-white xl:p-8 p-2"
                         remarkPlugins={[remarkGfm]} 
                         rehypePlugins={[rehypeRaw]}
                         components={{
-                            h1: ({node, ...props}) => <h1 className="text-2xl lg:text-4xl font-bold xl:pb-10 py-4" {...props}/>,
+                            h1: ({node, ...props}) => <h1 className="text-3xl lg:text-4xl font-bold xl:pb-10 py-4" {...props}/>,
                             h2: ({node, ...props}) => <h2 className="text-md lg:text-xl font-medium xl:py-5 py-2" {...props}/>,
-                            table: ({node, ...props}) => <table className="border border-white/60 text-center mx-auto w-full bg-black/50" {...props}/>,
-                            tr: ({node, ...props}) => <tr className="border border-white/60 p-1" {...props}/>,
-                            td: ({node, ...props}) => <td className="border border-white/60 p-1" {...props}/>,
-                            pre: ({node, ...props}) => <pre className="bg-white/20 text-sm rounded-lg px-2 xl:p-2 whitespace-pre-wrap" {...props}/>,
+                            table: ({node, ...props}) => <table className=" text-center mx-auto w-full bg-white/20 rounded px-2 xl:p-2" {...props}/>,
+                            tr: ({node, ...props}) => <tr className="p-1 " {...props}/>,
+                            td: ({node, ...props}) => <td className="p-1" {...props}/>,
+                            th: ({node, ...props}) => <th className="p-1 py-3 border-b-2 border-white/20 bg-white/10" {...props}/>,
+                            pre: ({node, ...props}) => <pre className="bg-white/20 text-sm rounded px-2 xl:p-2 whitespace-pre-wrap" {...props}/>,
                         }}
                     >
                         {data}
                     </ReactMarkdown>
-                ) : "Loading markdown..."
+                ) : (
+                    <div>Loading markdown...</div>
+                )
             }
         </div>
     )
