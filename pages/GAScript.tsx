11f3f96edf12851f014141f68abeb302f5e3eb15
Emigrek <kaioshinbox@gmail.com>
2023-02-15 15:23:59 +0100

    fallback to html script tag when dealing with Google Analytics

--- pages/GAScript.tsx
+++ pages/GAScript.tsx
@@ -1,13 +1,10 @@
-import Script from 'next/script'
 import React from 'react'
 
 function GAScript() {
   return (
     <>
-        <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}/>
-        <Script
-            id='google-analytics'
-            strategy="afterInteractive"
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}/>
+        <script
             dangerouslySetInnerHTML={{
                 __html: `
                     window.dataLayer = window.dataLayer || [];
