694143f6dfe897732d345de3dc66927ee739c2a4
emigrek <jarasz.karol@gmail.com>
2023-06-15 04:54:56 +0200

    markdown components fix not visibile in production

--- components/screens/projects/Readme.tsx
+++ components/screens/projects/Readme.tsx
@@ -6,7 +6,6 @@ import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import rehypeRaw from 'rehype-raw'
 import Spinner from '@/components/ui/Spinner/Spinner'
-import markdownComponents from '@/utils/markdownComponents'
 
 const Readme: FC = () => {
     const projectIframe = useRecoilValue(projectIframeState);
@@ -42,7 +41,17 @@ const Readme: FC = () => {
                             className="w-full p-2 text-white"
                             remarkPlugins={[remarkGfm]}
                             rehypePlugins={[rehypeRaw]}
-                            components={markdownComponents}
+                            components={{
+                                h1: ({ node, ...props }) => <h1 className="py-10 text-3xl font-bold lg:text-4xl" {...props} />,
+                                h2: ({ node, ...props }) => <h2 className="py-2 font-medium text-md lg:text-xl xl:py-5" {...props} />,
+                                table: ({ node, ...props }) => <table className="w-full px-2 overflow-hidden text-center border-collapse rounded-lg xl:p-2" {...props} />,
+                                thead: ({ node, ...props }) => <thead className="border-b-2 bg-white/20 border-white/20 text-neutral-50" {...props} />,
+                                tbody: ({ node, ...props }) => <tbody className="bg-white/10" {...props} />,
+                                tr: ({ node, ...props }) => <tr className="p-1 text-neutral-200" {...props} />,
+                                td: ({ node, ...props }) => <td className="p-1" {...props} />,
+                                th: ({ node, ...props }) => <th className="p-1 py-3" {...props} />,
+                                pre: ({ node, ...props }) => <pre className="p-2 my-2 text-sm whitespace-pre-wrap rounded-lg bg-white/10 text-neutral-200"  {...props} />
+                            }}
                         >
                             {data}
                         </ReactMarkdown>
